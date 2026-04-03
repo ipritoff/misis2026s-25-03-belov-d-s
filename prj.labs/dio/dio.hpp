03966122a08227e06130ce0cd3d896faca04b07a
ipritoff <d9688768812@gmail.com>
2026-04-03 11:50:15 +0300

    Create dio.hpp

---
+++ prj.labs/dio/dio.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef DIO_DIOSTRB_HPP_20022026
+#define DIOSTRB_DIOSTRB_HPP_20022026
+
+
+#include <string>
+#include <iosfwd>
+#include <ostream>
+
+class DioStrB
+{
+public:
+	DioStrB();
+	DioStrB(const std::string& str)
+		: value(str)
+	{}
+
+	std::string& val()
+		const std::string& val() const
+
+		friend std::ostream& operator<<(std::ostream& output_s, const DioStrB& str);
+	
+	void read(std::istream& input_s) 
+	void write(std::ostream& output_s) const
+	
+
+
+	~DioStrB();
+
+private:
+	std::string value;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
